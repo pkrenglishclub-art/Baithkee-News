0b996fab4d3b15168ce86c0045eda7b85b73dd78
pkrenglishclub-art <pkrenglishclub@gmail.com>
2026-05-06 13:59:00 +0530

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,13 +1,1 @@
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "715b3a5f-6885-4d7f-a70d-74e23562c43b",
-      safari_web_id: "web.onesignal.auto.01d22b73-bccb-4a84-9633-69c73285f3f4",
-      notifyButton: {
-        enable: true,
-      },
-    });
-  });
-</script>
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
